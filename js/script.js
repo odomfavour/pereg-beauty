4376c2bc697e8059a86d0fd9d959d864ade1b90d
odomfavour <ogochukwuodom@gmail.com>
2020-08-21 15:16:17 +0100

    chore: added header and footer template

---
+++ js/script.js
@@ -0,0 +1,48 @@
+//To load template for header and footer
+const header = document.querySelector('#header'),
+    footer = document.querySelector('#footer');
+let argUrl = window.location.pathname;
+
+document.addEventListener('DOMContentLoaded', loadDoc);
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'template/header.html', true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            header.innerHTML = this.responseText;
+
+            switch (argUrl) {
+                case '/index.html':
+                    header.querySelector('#home').classList.add('active');
+                    break;
+                case '/about.html':
+                    header.querySelector('#about').classList.add('active');
+                    break;
+                case '/products-services.html':
+                    header.querySelector('#tracking').classList.add('active');
+                    break;
+                case '/categories.html':
+                    header.querySelector('#categories').classList.add('active');
+                    break;
+                case '/contact.html':
+                    header.querySelector('#contact').classList.add('active');
+                    break;
+                case '/login.html':
+                    header.querySelector('#login').classList.add('active');
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    };
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'template/footer.html', true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            footer.innerHTML = this.responseText;
+        }
+    };
+}
